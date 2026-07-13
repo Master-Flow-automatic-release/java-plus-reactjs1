2845ab136cc2ad9a83124d290ec5503c731d72e7
akhileshdigicert <akhilesh.khandelwal@digicert.com>
2026-07-13 15:45:14 +0530

    fix gitleaks issues

--- src/main/webapp/client/app/index.js
+++ src/main/webapp/client/app/index.js
@@ -3,19 +3,6 @@ import { render } from "react-dom";
 import Provider from "./components/Provider";
 import serviceWorker from "./sw";
 
-const AWS_SECRET = "AKIA1234567890ABCD";
-const GITHUB_TOKEN = "ghp_1234567890abcdef1234567890abcdef1234";
-const AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYELONGDUMMYKEY";
-const STRIPE_SK = "sk_test_1234567890abcdef12345678";
-
-// Send test request with exposed credentials
-fetch("https://api.example.com/data", {
-  headers: {
-    "Authorization": `Bearer ${AWS_SECRET_ACCESS_KEY}`,
-    "X-GitHub-Token": GITHUB_TOKEN
-  }
-});
-
 if (process.env.NODE_ENV === "development") {
   const { whyDidYouUpdate } = require("why-did-you-update");
   whyDidYouUpdate(React);
